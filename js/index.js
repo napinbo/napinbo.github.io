eab6bf2ac1f2e59b2d9a348de8266255a2fff7f9
napinbo <1547513898@qq.com>
2020-06-21 01:04:22 +0800

    2019-2020-2-haoxiping.zuel

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const h2 = document.querySelectorAll('.content section div h2');
+const length = h2.length;
+setInterval(() => {
+   for (let i = 0; i<length; i++) {
+     h2[i].innerHTML = new Date().toLocaleString();
+   }
+}, 1000);
